4584667b1194971c29ff632704ea813035f33e3a
gituser123290 <naurangilal9675329115@gmail.com>
2025-02-01 01:37:09 +0530

    deployment-1

--- userchoice/authApp/views.py
+++ userchoice/authApp/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication,TokenAuthentication
 from .serializers import RegisterSerializer,UserProfileSerializer,UserProfileUpdateSerializer
 from django.contrib.auth import authenticate
 from rest_framework import generics
@@ -22,6 +23,8 @@ def logout_view(request):
     return Response(status=status.HTTP_401_UNAUTHORIZED)   
 
 class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    authentication_classes = []
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
 
