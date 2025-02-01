c76c981c9d95b1c75452607c0a45e0e618d2ec25
gituser123290 <naurangilal9675329115@gmail.com>
2025-02-01 11:00:01 +0530

    deployment

--- userchoice/userchoice/settings.py
+++ userchoice/userchoice/settings.py
@@ -141,5 +141,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "https://music-website-uhdd.onrender.com"
+    "https://musicwebsite-1.onrender.com"
 ]
